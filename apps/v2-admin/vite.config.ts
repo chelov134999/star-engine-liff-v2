6b0d677d6ae9195dca161f09c85b5a4bf4e798d2
楊量鈞 <md@yangliangjundeMac-mini.local>
2025-11-07 17:34:31 +0800

    chore: set vite base paths for gh pages

--- apps/v2-admin/vite.config.ts
+++ apps/v2-admin/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/star-engine-liff-v2/apps/v2-admin/',
   plugins: [react()],
   optimizeDeps: {
     include: ['@supabase/supabase-js', '@line/liff'],
